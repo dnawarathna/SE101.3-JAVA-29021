15aaf860d7c5b63b06a678a095016546f865c926
dnawarathna <140488599+dnawarathna@users.noreply.github.com>
2023-07-26 10:10:35 +0530

    Add files via upload

---
+++ prac2/Item.java
@@ -0,0 +1,29 @@
+package com.mycompany.item;
+
+public class Item {
+    private int location;
+    private String description;
+    
+    
+    public Item (int a,String b)
+    {
+        location=a;
+        description=b;
+    }
+    
+    public void display()
+    {
+        System.out.println("location is "+location+"");
+        System.out.println("description is "+description+"");
+        
+    }
+            
+}         
+            
+            
+            
+       
+
+
+    
+  
---
+++ prac2/item1.java
@@ -0,0 +1,12 @@
+package com.mycompany.item;
+public class item1
+{
+    public static void main (String[]args)
+    {
+        Item b1=new Item(10,"pen");
+        b1.display();
+    }
+    
+}
+    
+
